7f593bccc954dec0b9a098f300452ca3cbe7d3a4
fyrepenguin <mouryaprasanthp@gmail.com>
2023-03-31 11:18:19 +0530

    fix: skip type to be included in package exports

--- libs/scooter-embed/src/lib/apis/iframely.js
+++ libs/scooter-embed/src/lib/apis/iframely.js
@@ -3,7 +3,6 @@ import { IFRAMELY_BASE_URL } from "@factly/scooter-shared-utils";
 import axios from "axios";
 
 export const getIframelyData = (url, iframelyUrl) => {
-  // console.log(url);
   return axios.get(`${iframelyUrl || IFRAMELY_BASE_URL}`, {
     withCredentials: true,
     params: {
