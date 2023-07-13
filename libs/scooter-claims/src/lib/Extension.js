87854c73b901afaa6b1d33ed20b59c1656989dab
surajmn1 <suraj.natraj@factly.in>
2023-07-13 13:48:55 +0530

    fix getHTML issue

--- libs/scooter-claims/src/lib/Extension.js
+++ libs/scooter-claims/src/lib/Extension.js
@@ -17,7 +17,7 @@ export const ClaimsExtension = Node.create({
   },
 
   renderHTML({ HTMLAttributes }) {
-    return ["claims", mergeAttributes(HTMLAttributes)];
+    return ["claims", mergeAttributes(HTMLAttributes),0];
   },
 
   addNodeView() {
