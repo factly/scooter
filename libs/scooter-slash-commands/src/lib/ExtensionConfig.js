6757876cd5fef845232ad2c17af26b26fb9d9d68
surajmn1 <suraj.natraj@factly.in>
2023-08-22 11:56:34 +0530

    fix name warning

--- libs/scooter-slash-commands/src/lib/ExtensionConfig.js
+++ libs/scooter-slash-commands/src/lib/ExtensionConfig.js
@@ -78,6 +78,7 @@ export const SlashCommandsExtension = {
     }
    
     return Extension.create({
+      name: "SlashCommandsExtension",
       addOptions() {
         return {
           HTMLAttributes: {
