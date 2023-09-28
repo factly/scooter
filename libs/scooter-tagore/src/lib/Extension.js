5bd5f174f0a66befd0ca306f9eb210ab9b82c782
surajmn1 <suraj.natraj@factly.in>
2023-09-28 15:34:23 +0530

    fix: Space shortcut adds ai node when not required

--- libs/scooter-tagore/src/lib/Extension.js
+++ libs/scooter-tagore/src/lib/Extension.js
@@ -40,8 +40,8 @@ export const TagoreCommandsExtension = Node.create({
   },
   addKeyboardShortcuts() {
     return {
-      Space: () => {
-        return this.editor.commands.setTagoreContent();
+      "Space": () => {
+        if (this.editor.view.state.selection.$head.parent.textContent==='')  return this.editor.commands.setTagoreContent();
       },
     };
   },
